f426d632bc27230f9742b9344d8840ee6ad2afd2
Dillon Hess <dhess268@gmail.com>
2022-03-24 18:24:23 -0400

    more code added from javascript.info problems

---
+++ info-js-solutions.js
@@ -0,0 +1,35 @@
+
+// function decreasingOrder(arra) {
+//     arra.sort( (a,b) => b-a)
+// }
+
+// let arr = [5, 2, 1, -10, 8];
+// decreasingOrder(arr)
+// alert( arr ); // 8, 5, 2, 1, -10
+
+// arr = ["HTML", "JavaScript", "CSS"];
+// function copySorted(arr) {
+//     newArr = arr.map(a => a).sort()
+//     return newArr
+// }
+
+// copySorted(arr)
+// alert(newArr)
+
+function Calculator() {
+
+    '+': ((a,b) => a + b),
+
+
+    calculate(str){
+        noSpaces = str.split(' ')
+        result = self[noSpaces[1](noSpaces[0], noSpaces[2])]
+    }
+}
+
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+
+// WIP
